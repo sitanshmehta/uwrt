0c6c9c2c6bf8a81e03e5da41b22cba2df002f2b3
sitanshmehta <s65mehta@uwaterloo.ca>
2025-03-11 22:52:29 -0400

    clean up

--- keyboard.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-path = "/Users/sitanshmehta/Desktop/Keyboard Detection v2.v1i.yolov11/data.yaml"
-
-model = YOLO("yolo11n.pt") 
-
-results = model.train(data=path, epochs=20, imgsz=640, device='cpu')
